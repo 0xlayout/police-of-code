7f8748d73e200fe49e49b68811a74cd2777b6f9d
synthora-lab <elbrocotop2@proton.me>
2025-12-17 23:49:55 +0100

    Register and export all built-in analysis rules

---
+++ src/rules/index.js
@@ -0,0 +1,11 @@
+import hardcodedSecrets from "./hardcodedSecrets.js"
+import emptyCatch from "./emptyCatch.js"
+import sqlInjection from "./sqlInjection.js"
+import longFunctions from "./longFunctions.js"
+
+export default [
+  hardcodedSecrets,
+  emptyCatch,
+  sqlInjection,
+  longFunctions
+]
